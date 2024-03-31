96c64a8441df3615c1270ddd7c92be7343061002
matheustsnts <msteixeira.messeger@hotmail.com>
2024-03-31 14:12:03 -0300

    feat: utilização inicial do react na aula01.

--- aula01/hello-world/src/App.js
+++ aula01/hello-world/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React!</h1>
+      <h1 className="AppTitleH1">Hello World React!</h1>
     </div>
   );
 }
 
 export default App;
+
