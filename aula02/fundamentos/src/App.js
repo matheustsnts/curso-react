6671e1cfb8dc1766ebf9fb1f206467a89f45d646
matheustsnts <msteixeira.messeger@hotmail.com>
2024-03-31 14:14:07 -0300

    feat: criação da aula02 em react.

---
+++ aula02/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+// Components
+import FirstComponent from './components/FirstComponent';
+
+// Import styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+    </div>
+  );
+}
+
+export default App;
