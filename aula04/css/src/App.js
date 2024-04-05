a87b7217e8bd10b35d944d1352856227584d46de
matheustsnts <msteixeira.messeger@hotmail.com>
2024-04-05 14:23:31 -0300

    feat: iniciação do projeto aula04/css-inline.

--- aula04/css/src/App.js
+++ aula04/css/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import { useState } from 'react';
 
 function App() {
+
+  const n = 15;
+  const [name] = useState("Matheus")
+
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
+      <h2 style={{
+        color: "green",
+        backgroundColor: "#000",
+        
+      }}></h2>
     </div>
   );
 }
