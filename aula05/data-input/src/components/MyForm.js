b3bd464dfe7113d865178ef95cad5182d02e332e
matheustsnts <msteixeira.messeger@hotmail.com>
2024-04-12 22:21:19 -0300

    feat: atualização da aula05/data-input.

--- aula05/data-input/src/components/MyForm.js
+++ aula05/data-input/src/components/MyForm.js
@@ -1,16 +1,26 @@
 import style from './MyForm.css'
+import {useState} from 'react';
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
   return (
     <div>
       {/* 1 - criação de form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome"/>
-          <label htmlFor="name">E-mail:</label>
-          <input type="text" name="name" placeholder="Digite seu e-mail"/>
+          <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}/>
+          <label htmlFor="email">E-mail:</label>
+          {/* <input type="text" name="email" placeholder="Digite seu e-mail" onChange={handleName}/> */}
         </div>
+        {/* 2 - label envolvendo input */}
         <input type="submit" value="Enviar"/>
       </form>
     </div>
